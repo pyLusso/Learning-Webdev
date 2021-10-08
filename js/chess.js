d4bdd05f344fd80fbccf62107b0a33ce4f2bfd58
pyLusso <lucarusso.521@gmail.com>
2021-10-08 14:10:58 +1100

    Placed all pieces with correct classIDs for each div

--- js/chess.js
+++ js/chess.js
@@ -1,3 +1,8 @@
+/*
+    In-browser chess implementation by Luca Russo.
+    Some implementation details inspired by chess.com
+*/
+
 
 $(function ($) {
     $.fn.generate_board = function (options) {
@@ -35,14 +40,6 @@ $(function ($) {
 
         for (let j = 0; j < settings["board-width"]; j++) {
             for (let i = 0; i < settings["board-height"]; i++) {
-                // if (i == 0) {
-                //     let d = document.createElement("div");
-                //     d.id = "file-" + (j + 1)
-                //     d.className = "board-tile"
-                //     // assign_colours(d,i,j)
-                //     var new_div = board_div.appendChild(d); 
-                // }
-                // var div = new_div
                 make_board_tile(board_div,i,j)
             }
         }
@@ -50,37 +47,76 @@ $(function ($) {
     }
 
     $.fn.populate_board = function () {
-        var fileDivs = document.getElementById('gameboard').getElementsByTagName('div');
+        var tileDivs = document.getElementById('gameboard').getElementsByTagName('div');
 
-        for( i=0; i< fileDivs.length; i++ ) {
-            var fileDiv = fileDivs[i];
-            var rankDivs = fileDiv.getElementsByTagName('div');
-            for( j=0; j< rankDivs.length; j++ ) {  
-                var rankDiv = rankDivs[j];
-                console.log(j)
-
+        for( i=0; i< tileDivs.length; i++ ) {
+            var tileDiv = tileDivs[i];
+            // console.log(i)
+            // console.log(tileDiv)
+            // Position black pieces
+            if (i==0) {
+                tileDiv.classList.add(["piece", "br"])
+                tileDiv.style.backgroundImage = "url('assets/chess/b_rook.png')";
+            } else if (i==1) {
+                tileDiv.classList.add(["piece", "bn"])
+                tileDiv.style.backgroundImage = "url('assets/chess/b_knight.png')";
+            } else if (i==2) {
+                tileDiv.classList.add(["piece", "bb"])
+                tileDiv.style.backgroundImage = "url('assets/chess/b_bish.png')";
+            } else if (i==3) {
+                tileDiv.classList.add(["piece", "bq"])
+                tileDiv.style.backgroundImage = "url('assets/chess/b_queen.png')";
+            } else if (i==4) {
+                tileDiv.classList.add(["piece", "bk"])
+                tileDiv.style.backgroundImage = "url('assets/chess/b_king.png')";
+            } else if (i==5) {
+                tileDiv.classList.add(["piece", "bb"])
+                tileDiv.style.backgroundImage = "url('assets/chess/b_bish.png')";
+            } else if (i==6) {
+                tileDiv.classList.add(["piece", "bn"])
+                tileDiv.style.backgroundImage = "url('assets/chess/b_knight.png')";
+            } else if (i==7) {
+                tileDiv.classList.add(["piece", "br"])
+                tileDiv.style.backgroundImage = "url('assets/chess/b_rook.png')";
+            } else if (i>=8 && i<16) { 
+                tileDiv.classList.add(["piece", "bp"])
+                tileDiv.style.backgroundImage = "url('assets/chess/b_pawn.png')";
             }
+
+            // Position black pieces
+            reverse_i = tileDivs.length - (i+1)
+            console.log(reverse_i)
+            console.log(tileDiv)
+            if (reverse_i==0) {
+                tileDiv.classList.add(["piece", "wr"])
+                tileDiv.style.backgroundImage = "url('assets/chess/w_rook.png')";
+            } else if (reverse_i==1) {
+                tileDiv.classList.add(["piece", "wn"])
+                tileDiv.style.backgroundImage = "url('assets/chess/w_knight.png')";
+            } else if (reverse_i==2) {
+                tileDiv.classList.add(["piece", "wb"])
+                tileDiv.style.backgroundImage = "url('assets/chess/w_bish.png')";
+            } else if (reverse_i==3) {
+                tileDiv.classList.add(["piece", "wq"])
+                tileDiv.style.backgroundImage = "url('assets/chess/w_queen.png')";
+            } else if (reverse_i==4) {
+                tileDiv.classList.add(["piece", "wk"])
+                tileDiv.style.backgroundImage = "url('assets/chess/w_king.png')";
+            } else if (reverse_i==5) {
+                tileDiv.classList.add(["piece", "wb"])
+                tileDiv.style.backgroundImage = "url('assets/chess/w_bish.png')";
+            } else if (reverse_i==6) {
+                tileDiv.classList.add(["piece", "wn"])
+                tileDiv.style.backgroundImage = "url('assets/chess/w_knight.png')";
+            } else if (reverse_i==7) {
+                tileDiv.classList.add(["piece", "wr"])
+                tileDiv.style.backgroundImage = "url('assets/chess/w_rook.png')";
+            } else if (reverse_i>=8 && reverse_i<16) { 
+                tileDiv.classList.add(["piece", "wp"])
+                tileDiv.style.backgroundImage = "url('assets/chess/w_pawn.png')";
+            }
+
+            //position white pieces
         }
     }
 })(jQuery);
-
-            //     if (j == 0 && i == 0) {
-            //         let img = document.createElement("img");
-            //         img.src = "assets/chess/b_rook.png";
-            //         img.className = "piece"
-            //         rankDiv.appendChild(img);
-            //         rankDiv.classList.add("br")
-            //     } else if (j == 1) {
-            //         let img = document.createElement("img");
-            //         img.src = "assets/chess/b_pawn.png";
-            //         img.className = "piece"
-            //         rankDiv.appendChild(img);
-            //         rankDiv.classList.add("bp")
-            //     } else if (j == 6) {
-            //         let img = document.createElement("img");
-            //         img.src = "assets/chess/w_pawn.png";
-            //         img.className = "piece"
-            //         rankDiv.appendChild(img);
-            //         rankDiv.classList.add("wp")
-            //     }
-            // }

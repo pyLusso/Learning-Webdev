e1536bbd168f55aacf343a2738c5e3d3094b46b5
pyLusso <lucarusso.521@gmail.com>
2021-10-11 00:11:16 +1100

    Added wallet statistics section after connecting to metamask wallet

--- js/defi.js
+++ js/defi.js
@@ -1,13 +1,48 @@
+// async function getAccount() {
+//     const accounts = await ethereum.request({ method: "eth_requestAccounts"});
+//     const account = accounts[0];
+
+//     return account
+// }
 
 async function getAccount() {
+    // const accounts = web3.eth.accounts;
     const accounts = await ethereum.request({ method: "eth_requestAccounts"});
-    const account = accounts[0];
-    console.log(account);
+    return accounts[0]
+
+
+
+    // } catch(err) {
+    //     console.log(err);
+    // }
+
+    // const account = accounts[0];
+
+    // wallet_addr = String(account)
+    // var wallet_balance = await web3.eth.getBalance(wallet_addr)
+    // console.log(wallet_addr)
+    // console.log(wallet_balance)
+
+
+
+
+    // return Promise.resolve()
+    // return new Promise((resolve, reject) => {
+    //     resolve("Wallet address found!");
+    //     reject("Couldn't find wallet address!");
+    // })
 }
 
-function web3_demo() {
-    console.log("Clicked");
-    console.log(typeof window.ethereum);
+async function getBalance(addr) {
+    console.log(addr)
+    wallet_balance = web3.eth.getBalance(addr);
+    return wallet_balance
+
+
+}
+
+
+async function connect_wallet() {
 
     if (typeof window.ethereum !== 'undefined') {
         console.log("Metamask is installed!");
@@ -17,38 +52,59 @@ function web3_demo() {
     var Web3RequireCallback = requirejs(['web3']);
 
     var Web3 = Web3RequireCallback('web3');
-
     var web3 = new Web3("https://mainnet.infura.io/v3/93498ec03efb4660b6f7fa3adfe9bd98");
 
-    const ethEnabled = async () => {
-        if (window.ethereum) {
-            web3 = new Web3(window.ethereum);
-            await ethereum.enable();
-        } else if (window.web3) {
-            web3 = new Web3(window.web3.currentProvider);
-        }
+    console.log(web3);
+
+
+    getAccount().then(value => {
+        console.log("Address: " + value);
+        addr_line = "Address: " + value
+
+        balance = web3.eth.getBalance(value);
+        balance.then(result => {
+            console.log(result);
+            var text = document.querySelector('#defi h5');
+            if (text !== null) {
+                console.log('<p> exists in <div>');
+            } else {
+                console.log('<p> does not exists in <div> yet.');
+                
+                var div = document.getElementById('defi');
+                let h = document.createElement("h5");
+                
+                
+                balance_line = "Balance: " + result + " ETH";
+                lines = [addr_line, balance_line].join("<br>");
+
+                let new_div = div.appendChild(h);
+                $(new_div).append(lines);
+            }
+        });
+    });
+}
+
+
+function init_web3_provider() {
+    if (typeof window.ethereum !== 'undefined') {
+        console.log("Metamask is installed!");
+    } else {
+        console.log("Please install Metamask");
     }
-    ethEnabled()
-    getAccount()
-    // console.log(Web3.version)
-    // console.log(window)
-    // console.log(web3)
+    var Web3RequireCallback = requirejs(['web3']);
 
-    // console.log(web3.eth.getBlockNumber())
+    var Web3 = Web3RequireCallback('web3');
+    var web3 = new Web3("https://mainnet.infura.io/v3/93498ec03efb4660b6f7fa3adfe9bd98");
 
-    // const ethEnabled = async () => {
-    // if (window.ethereum) {
-    //     alert("we are in the loop")
-    //     console.log(window.ethereum)
-    //     await window.ethereum.send('eth_requestAccounts');
-    //     window.web3 = new Web3(window.ethereum);
-    //     return true;
-    // }
-    // return false;
-    }
+    console.log(web3)
 
+    // console.log(account)
+    // var addr;
+    // account.then((value) => {
+    //     addr = value; 
+    // });
 
-// import detectEthereumProvider from '@metamask/detect-provider';
+}
 
 var requirejs = require('requirejs');
 
